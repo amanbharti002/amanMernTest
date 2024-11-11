52fce5b633737fa7665585d37ad7df2871513ac1
amankumar <amanbharti822002@gmail.com>
2024-11-11 11:08:00 +0530

    test

--- src/controller/AgentsController.js
+++ src/controller/AgentsController.js
@@ -1,3 +1,4 @@
+
 const AgenModel = require('../model/AgentsModel');
 const {ObjectId} =require('mongodb')
 exports.createAgent = async (req, res, next) => {
@@ -33,3 +34,36 @@ exports.createAgent = async (req, res, next) => {
     }
 }
 
+exports.getAgents = async(req, res, next)=>{
+    try{
+        const query ={adminid:req.body.superadminid}
+        console.log( query)
+    const resData = await AgenModel.aggregate([
+        {$match:{superadminid:new ObjectId(query.adminid)}},
+        {$lookup:{
+            from:'superadmins',
+            localField:'superadminid',
+            foreignField:'_id',
+            as:'agents'
+        }}
+    ])
+    console.log(resData)
+    if(resData){
+        res.json({
+            status:'success',
+            data:resData
+        })
+    }else{
+        res.json({
+            stuts:'failed',
+            message:"faile to find agents"
+        })
+    }
+    }catch(err){
+        res.json({
+            status:'faild',
+            message:"unable to find agents",
+            error:err
+        })
+    }
+}
--- src/router/AgentsRouter.js
+++ src/router/AgentsRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { createAgent } = require('../controller/AgentsController');
+const { createAgent, getAgents } = require('../controller/AgentsController');
 const AgentsRouter = express.Router()
 AgentsRouter.post('/create',createAgent);
+AgentsRouter.get('/getagent',getAgents);
 module.exports = AgentsRouter
